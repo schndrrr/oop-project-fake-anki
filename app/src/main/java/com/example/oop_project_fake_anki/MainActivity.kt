4fe790edadc5db95b8c57a68b2ddf68d7565ba0d
schndrrr <johann.schneider90@hotmail.de>
2021-05-13 14:12:46 +0200

    changed build gradle and added basic routing

--- app/src/main/java/com/example/oop_project_fake_anki/MainActivity.kt
+++ app/src/main/java/com/example/oop_project_fake_anki/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.oop_project_fake_anki
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.layout.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +13,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun test(view: View) {
+        // print line to terminal
         println("hallo")
+        val buttonText: String = "Karten"
+        // change text of textview
+        textView_test.text = "Moin Moin du landratte"
+        // Route to new site
+        setContentView(R.layout.layout)
+        textView_newSite.text = "Ja und das geht auch"
     }
 }
