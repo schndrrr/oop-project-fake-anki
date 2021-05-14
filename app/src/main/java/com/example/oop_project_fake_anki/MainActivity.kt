e72534564bcd357bfe30dac2630f255a795dafe2
schneikel <nikdreger@googlemail.com>
2021-05-14 15:21:37 +0200

    Rückseite der Karteikarte mit 3 Buttons

--- app/src/main/java/com/example/oop_project_fake_anki/MainActivity.kt
+++ app/src/main/java/com/example/oop_project_fake_anki/MainActivity.kt
@@ -4,15 +4,23 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_backside_of_card.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 
     fun routeToNewCard(view: View) {
         val intent = Intent(this, CreateCard::class.java)
+        startActivity(intent)
+    }
+    fun routeToNextCard(view: View) {
+        val intent = Intent (this, GetNextCard::class.java)
         startActivity(intent)
     }
 }
