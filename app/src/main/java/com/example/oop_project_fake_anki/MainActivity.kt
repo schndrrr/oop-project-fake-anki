114a959ea617d396863407dfbc4319ab279c3e58
schndrrr <johann.schneider90@hotmail.de>
2021-05-14 11:02:07 +0200

    added first button

--- app/src/main/java/com/example/oop_project_fake_anki/MainActivity.kt
+++ app/src/main/java/com/example/oop_project_fake_anki/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.oop_project_fake_anki
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun routeToNewCard(view: View) {
+        val intent = Intent(this, CreateCard::class.java)
+        startActivity(intent)
     }
 }
