475a71b0a1dd3a87b3e2196224898b6d6e33a4df
schndrrr <johann.schneider90@hotmail.de>
2021-05-14 11:54:16 +0200

    added getData

--- app/src/main/java/com/example/oop_project_fake_anki/MainActivity.kt
+++ app/src/main/java/com/example/oop_project_fake_anki/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.oop_project_fake_anki
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val s = Storage()
+        s.getData(applicationContext, "test.json")
     }
 }
--- app/src/main/java/com/example/oop_project_fake_anki/Storage.kt
+++ app/src/main/java/com/example/oop_project_fake_anki/Storage.kt
@@ -1,11 +1,24 @@
 package com.example.oop_project_fake_anki
+import android.content.Context
+import java.io.IOException
 
 class Storage {
 
+    fun getData(context: Context, fileName: String): String? {
+        val jsonString: String
+        try {
+            jsonString = context.assets.open(fileName).bufferedReader().use { it.readText() }
+        } catch (ioException: IOException) {
+            ioException.printStackTrace()
+            return null
+            return jsonString
+        }
+    }
 
-    fun getData() {
+    fun parseData(jsonString: String) {
 
     }
+
 
     fun saveData() {
 
