4cc03ddd1bb54019b92c26e68cec2ad5a2fbbbb7
schndrrr <johann.schneider90@hotmail.de>
2021-05-27 12:16:29 +0200

    added navController

--- app/src/main/java/com/example/oop_project_fake_anki/MainActivity.kt
+++ app/src/main/java/com/example/oop_project_fake_anki/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.oop_project_fake_anki
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun test(view: View) {
+        view.findNavController().navigate(R.id.homeScreen)
+    }
+
+    fun test2(view: View) {
+        view.findNavController().navigate(R.id.createCard)
+    }
+
+    fun homeScreen(view: View) {
+        view.findNavController().navigate(R.id.homeScreen)
     }
 }
