d0ccf69894053e55549f547994e879ba1ae9675c
schndrrr <johann.schneider90@hotmail.de>
2021-06-11 16:09:14 +0200

    added firestore instance

--- app/src/main/java/com/example/oop_project_fake_anki/MainActivity.kt
+++ app/src/main/java/com/example/oop_project_fake_anki/MainActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val db = FirebaseFirestore.getInstance()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
