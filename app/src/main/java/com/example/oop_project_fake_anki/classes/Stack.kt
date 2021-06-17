cf74dab57684fbe3922884224e21c48c5dd59b68
jni <johanngeorg.nitsche@yahoo.com>
2021-06-17 00:30:13 -0700

    added stack color

--- app/src/main/java/com/example/oop_project_fake_anki/classes/Stack.kt
+++ app/src/main/java/com/example/oop_project_fake_anki/classes/Stack.kt
@@ -2,5 +2,6 @@ package com.example.oop_project_fake_anki.classes
 
 class Stack(var stackId: String = "", var name: String = "", var numberOfCards: String = "") {
     // TODO
+    var stackcolor="default"
     var cards: MutableList<Card> = mutableListOf<Card>()
 }
