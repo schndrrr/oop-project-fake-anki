71168d8f1496b2fa8dfcb631ee28f82b1d9245b1
Johann Schneider <johannschneider@Johanns-MBP.fritz.box>
2021-06-24 16:08:00 +0200

    tried to implement spinner

--- app/src/main/java/com/example/oop_project_fake_anki/createCard.kt
+++ app/src/main/java/com/example/oop_project_fake_anki/createCard.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.example.oop_project_fake_anki.classes.DefaultCard
+import com.example.oop_project_fake_anki.classes.Stack
+import com.example.oop_project_fake_anki.utility.Storage
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_create_card.*
 import kotlinx.android.synthetic.main.fragment_create_card.view.*
 import kotlinx.android.synthetic.main.fragment_show_card.view.*
@@ -22,17 +28,14 @@ private const val ARG_PARAM2 = "param2"
  * Use the [createCard.newInstance] factory method to
  * create an instance of this fragment.
  */
-class createCard : Fragment(), View.OnClickListener {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class createCard : Fragment(), View.OnClickListener, AdapterView.OnItemSelectedListener {
+
+    private lateinit var db: FirebaseFirestore
+    private lateinit var stacks: MutableList<Stack>
+    private lateinit var spinner: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -49,7 +52,28 @@ class createCard : Fragment(), View.OnClickListener {
         btn_home.setOnClickListener(this)
         btn_createCard.setOnClickListener(this)
 
+        spinner = view.findViewById(R.id.spinner_select_stack)
+        stacks = mutableListOf()
+        stacks.add(Stack("test", "name"))
+        stacks.add(Stack("tsdest", "nameeeen"))
+        val stacksList = stacks.map { it.name }
+
+        val adapter: ArrayAdapter<String> = ArrayAdapter<String>(
+            requireActivity(),
+            android.R.layout.simple_spinner_item, stacksList
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        EventChangeListener()
+
         return view
+    }
+
+    private fun EventChangeListener() {
+        db = FirebaseFirestore.getInstance()
+        val storage: Storage = Storage(db)
+//        storage.getStacks(adapter, stacks)
+        println(stacks.size)
     }
 
     override fun onClick(v: View?) {
@@ -71,6 +95,14 @@ class createCard : Fragment(), View.OnClickListener {
                 println(card.answer)
             }
         }
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
     }
 
 }
